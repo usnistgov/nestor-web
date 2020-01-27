076b739b2c2f5a8916cbb6eb102952f7f1cafc91
cedric Bell <cedric.bell@nist.gov>
2020-01-27 14:43:50 -0500

    Simple Version of Alphabetic filter in search bar for tokens

--- src/components/TaggingTool/Tag/TagComponents/list.jsx
+++ src/components/TaggingTool/Tag/TagComponents/list.jsx
@@ -3,22 +3,33 @@ import { Modal, Button, InputGroup, FormControl } from "react-bootstrap";
 import text from "../../../../assets/language/en.js";
 import TagButton from "./tagButton";
 
-class List extends Component {
-  state = {
-    input: "",
-    list: this.props.list
-  };
-  render() {
+class List extends Component
+{
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      input: "",
+      list: this.props.list
+    };
+    this.handleFilter.bind(this);
+  }
+
+
+
+  render()
+  {
     return (
-      <Modal size="xl" show={this.props.showModal} onHide={this.props.onDelete}>
+      <Modal size="xl" show={ this.props.showModal } onHide={ this.props.onDelete }>
         {/*<Modal.Header closeButton>
                 <Modal.Title id="example-modal-sizes-title-sm">{text.taggingTool.tagging.singleToken.modal.title}</Modal.Title>
             </Modal.Header> */}
         <Modal.Body>
           <InputGroup className="mb-3">
             <FormControl
-              value={this.state.input}
-              onChange={e => this.handleChange(e)}
+              value={ this.state.input }
+              onChange={ e => this.handleChange(e) }
               placeholder={
                 text.taggingTool.tagging.singleToken.modal.placeholder
               }
@@ -31,39 +42,52 @@ class List extends Component {
               <Button variant="outline-secondary">
                 <i className="fas fa-search" />
               </Button>
+              <Button variant="outline-secondary" onClick={ this.handleFilter }>
+                <i className="fas  fa-filter" />
+              </Button>
             </InputGroup.Append>
           </InputGroup>
           <div className="list-body">
-            {this.state.list.map((obj, i) => (
-              <div key={i} className="list-item">
+            { this.state.list.map((obj, i) => (
+              <div key={ i } className="list-item">
                 <TagButton
-                  value={obj.label}
-                  shortkey={""}
-                  showTooltipIcon={false}
-                  tooltip={""}
-                  color={"#00a6ff"}
-                  onClick={() => this.props.onClick(obj)}
+                  value={ obj.label }
+                  shortkey={ "" }
+                  showTooltipIcon={ false }
+                  tooltip={ "" }
+                  color={ "#00a6ff" }
+                  onClick={ () => this.props.onClick(obj) }
                 />
               </div>
-            ))}
+            )) }
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={this.props.onDelete}>
-            {text.taggingTool.tagging.singleToken.modal.buttonLabel}
+          <Button variant="secondary" onClick={ this.props.onDelete }>
+            { text.taggingTool.tagging.singleToken.modal.buttonLabel }
           </Button>
         </Modal.Footer>
       </Modal>
     );
   }
-  handleChange = event => {
+  handleChange = event =>
+  {
     var input = this.state.input;
     input = event.target.value;
     this.setState({ input });
-    var tokens = [...this.props.list];
+    var tokens = [ ...this.props.list ];
     const list = tokens.filter(element => element.label.indexOf(input) !== -1);
     this.setState({ list });
   };
+
+  handleFilter = () =>
+  {
+    var keywords = [ ...this.props.list ];
+    const filteredList = keywords
+      .filter((keyword, index) => keywords.lastIndexOf(keyword) === index)
+      .sort((a, b) => a.label < b.label ? -1 : 1);
+    this.setState({ list: filteredList });
+  }
 }
 
 export default List;
